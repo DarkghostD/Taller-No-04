6919768df48cd6d3c9d4ac6dc63b43a0a645ec12
Ivan Ausecha <ivanausecha5@gmail.com>
2024-10-16 22:18:29 -0500

    Codigo Finalizado

--- src/main/scala/MatchingProblem/package.scala
+++ src/main/scala/MatchingProblem/package.scala
@@ -3,49 +3,40 @@ package object MatchingProblem {
   type Matching = List[Match]
   type Preferences = Vector[Vector[Int]]
 
-  // Función recursiva que genera las combinaciones de emparejamientos
   def recursion(i: Int, n: Int, current: List[Match]): List[List[Match]] = {
     if (i > n) {
-      List(current) // Devuelve la combinación actual cuando i excede n
+      List(current)
     } else {
-      // Genera una lista de combinaciones para el elemento i
       (1 to n).toList.flatMap { j =>
-        recursion(i + 1, n, current :+ (i, j)) // Llama recursivamente para el siguiente i
+        recursion(i + 1, n, current :+ (i, j))
       }
     }
   }
 
-  // Genera la lista de emparejamientos posibles para un piloto dado
   def matchByElement(i: Int, n: Int): List[Match] = {
     (1 to n).map(j => (i, j)).toList
   }
 
-  // Genera la lista de emparejamientos posibles por cada piloto
   def matchsByElements(n: Int): List[List[Match]] = {
     (1 to n).toList.map(i => matchByElement(i, n)) // Llama a matchByElement para cada piloto del 1 al n
   }
 
-  // Usa la función recursiva para generar todos los emparejamientos posibles
   def possibleMatchings(n: Int): List[List[Match]] = {
     recursion(1, n, List())
   }
 
-  // Devuelve la lista de todos los emparejamientos válidos (sin repetir copilotos)
   def matchings(n: Int): List[Matching] = {
     possibleMatchings(n).filter(matching => matching.map(_._2).distinct.length == n)
   }
 
-  // Calcula el peso de un emparejamiento dado
   def weight(matching: Matching, pilotPrefs: Preferences, navigPrefs: Preferences): Int = {
     matching.map { case (i, j) => pilotPrefs(i - 1)(j - 1) * navigPrefs(j - 1)(i - 1) }.sum
   }
 
-  // Devuelve la lista de todos los emparejamientos válidos con su peso
   def weightedMatchings(n: Int, pilotPrefs: Preferences, navigPrefs: Preferences): List[(Matching, Int)] = {
     matchings(n).map(matching => (matching, weight(matching, pilotPrefs, navigPrefs)))
   }
 
-  // Devuelve el emparejamiento válido de mayor peso
   def bestMatching(n: Int, pilotPrefs: Preferences, navigPrefs: Preferences): (Matching, Int) = {
     weightedMatchings(n, pilotPrefs, navigPrefs).maxBy(_._2)
   }
--- src/test/scala/Pruebas.sc
+++ src/test/scala/Pruebas.sc
@@ -1,32 +1,41 @@
 import MatchingProblem._
 
-val pilot = Vector(
-  Vector(2, 3, 1, 1),
-  Vector(1, 1, 4, 3),
-  Vector(1, 2, 3, 4),
-  Vector(2, 3, 2, 1)
-)
+val pilot = Vector(Vector(2, 3, 1, 1), Vector(1, 1, 4, 3), Vector(1, 2, 3, 4), Vector(2, 3, 2, 1))
 
-val navig = Vector(
-  Vector(4, 1, 3, 2),
-  Vector(4, 2, 3, 1),
-  Vector(1, 1, 1, 4),
-  Vector(3, 2, 3, 3)
-)
+val navig = Vector(Vector(4, 1, 3, 2),Vector(4, 2, 3, 1), Vector(1, 1, 1, 4), Vector(3, 2, 3, 3))
 
 matchByElement(2, 5)
 matchByElement(3, 5)
+matchByElement(4, 5)
+matchByElement(5, 5)
+matchByElement(1, 5)
+
+matchsByElements(3)
+matchsByElements(4)
 matchsByElements(5)
+matchsByElements(2)
+matchsByElements(6)
+
 possibleMatchings(2)
 possibleMatchings(3)
+possibleMatchings(4)
 possibleMatchings(5).length
+possibleMatchings(6).length
+
 matchings(2)
 matchings(3)
 matchings(4)
-matchings(5)
+matchings(5).length
+matchings(6).length
+
 weightedMatchings(2, pilot, navig)
 weightedMatchings(3, pilot, navig)
 weightedMatchings(4, pilot, navig)
+weightedMatchings(2, pilot, navig)
+weightedMatchings(3, pilot, navig)
+
 bestMatching(2, pilot, navig)
 bestMatching(3, pilot, navig)
 bestMatching(4, pilot, navig)
+bestMatching(2, pilot, navig)
+bestMatching(3, pilot, navig)
